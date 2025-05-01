2e5438b050c7cf5b5db8757cd451fe30eabd2356
themizzi <themizzi@me.com>
2025-05-01 18:55:21 -0500

    Initial Commit

---
+++ ChicagoFoodInspections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChicagoFoodInspections
+//
+//  Created by Joe Mizzi on 5/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
